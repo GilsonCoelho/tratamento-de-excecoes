1f7c930cb77d459141eb640b020a573f2f9170c4
GilsonCoelho <coelho.gilson@gmail.com>
2021-08-23 22:11:12 -0300

    Tipos de tratamento do pior para o melhor

---
+++ src/hotelSolucao3/model/exceptions/DomainException.java
@@ -0,0 +1,12 @@
+package hotelSolucao3.model.exceptions;
+
+public class DomainException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DomainException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
