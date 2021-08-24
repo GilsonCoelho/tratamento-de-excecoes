865a13f17c3749d991cca1dc4065f7d380159638
GilsonCoelho <coelho.gilson@gmail.com>
2021-08-24 18:18:48 -0300

    Exercicio resolvido

---
+++ src/exercicio/model/entities/Account.java
@@ -0,0 +1,65 @@
+package exercicio.model.entities;
+
+import hotelSolucao3.model.exceptions.DomainException;
+
+public class Account {
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+		
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+	
+	public void amount(double amount) {
+		
+		if (amount > getWithdrawLimit() ) {
+			throw new DomainException( "The amount exceeds withdraw limit");
+		}
+		if (amount > getBalance()) {
+			throw new DomainException( "Not enough balance");
+		}
+		this.balance = this.balance - amount;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "New balance: " + getBalance();
+	}
+	
+
+}
